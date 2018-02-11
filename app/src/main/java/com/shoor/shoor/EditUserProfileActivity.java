fe1a6cee891e745db06552c764f0c2f361930d2b
AlaaAlhelal <36053108+AlaaAlhelal@users.noreply.github.com>
2018-02-11 14:33:01 +0300

    add new activity

---
+++ app/src/main/java/com/shoor/shoor/EditUserProfileActivity.java
@@ -0,0 +1,13 @@
+package com.shoor.shoor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditUserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user_profile);
+    }
+}
