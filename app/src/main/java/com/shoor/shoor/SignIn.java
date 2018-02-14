2a68f90dc21d55a46fcc52e71d37c567e1826c02
AlaaAlhelal <36053108+AlaaAlhelal@users.noreply.github.com>
2018-02-14 15:42:01 +0300

    add new activity

--- app/src/main/java/com/shoor/shoor/SignIn.java
+++ app/src/main/java/com/shoor/shoor/SignIn.java
@@ -2,13 +2,19 @@ package com.shoor.shoor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignIn extends AppCompatActivity {
-
+    Button SignInButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+
+    public void signIn(View view) {
 
     }
 }
